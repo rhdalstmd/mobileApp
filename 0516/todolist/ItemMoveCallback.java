bb6b0e3304a654a60af1592eec4bfca2258af447
rhdalstmd <gwi5610@naver.com>
2025-06-09 15:41:17 +0900

    Create ItemMoveCallback.java

---
+++ 0516/todolist/ItemMoveCallback.java
@@ -0,0 +1,65 @@
+package com.example.todolist;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemMoveCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperContract adapter;
+
+    public interface ItemTouchHelperContract {
+        void onRowMoved(int fromPosition, int toPosition);
+        void onRowSelected(TodoAdapter.TodoViewHolder myViewHolder);
+        void onRowClear(TodoAdapter.TodoViewHolder myViewHolder);
+    }
+
+    public ItemMoveCallback(ItemTouchHelperContract adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return false;
+    }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof TodoAdapter.TodoViewHolder) {
+                adapter.onRowSelected((TodoAdapter.TodoViewHolder) viewHolder);
+            }
+        }
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    @Override
+    public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+        if (viewHolder instanceof TodoAdapter.TodoViewHolder) {
+            adapter.onRowClear((TodoAdapter.TodoViewHolder) viewHolder);
+        }
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        return makeMovementFlags(dragFlags, 0);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,
+                          @NonNull RecyclerView.ViewHolder target) {
+        adapter.onRowMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        // 우리는 스와이프 기능을 사용하지 않음
+    }
+}
